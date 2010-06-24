10250a424f231ed28a0b494927474b1cdb308e16
Jacob Hansson <jacob@bitchware.se>
2010-06-24 10:57:49 +0200

    initial commit

---
+++ src/main/java/org/neo4j/webadmin/Main.java
@@ -0,0 +1,17 @@
+package org.neo4j.webadmin;
+
+/**
+ * Main entry point for the neo4j stand-alone REST system with web administration. 
+ * 
+ * @author Jacob Hansson <jacob@voltvoodoo.com>
+ *
+ */
+public class Main {
+
+	public static void main(String [] args) throws Exception {
+		
+		System.out.println("Hello World!");
+		
+	}
+	
+}
