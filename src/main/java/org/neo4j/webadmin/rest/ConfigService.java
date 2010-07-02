f72521a0b08fed54315be108d76888ac34724302
Jacob Hansson <jacob@bitchware.se>
2010-07-02 16:28:18 +0200

    Updated config UI, working on support for changing settings

--- src/main/java/org/neo4j/webadmin/rest/ConfigService.java
+++ src/main/java/org/neo4j/webadmin/rest/ConfigService.java
@@ -2,7 +2,12 @@ package org.neo4j.webadmin.rest;
 
 import static org.neo4j.webadmin.rest.WebUtils.addHeaders;
 
+import java.util.HashMap;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -22,20 +27,48 @@ import org.neo4j.webadmin.domain.ServerProperties;
 public class ConfigService
 {
 
+    protected ServerProperties properties = ServerProperties.INSTANCE;
+
     /**
      * Get a full list of available settings.
-     * 
-     * @return
      */
     @GET
     @Produces( MediaType.APPLICATION_JSON )
-    public synchronized Response status()
+    public synchronized Response listAll()
     {
 
-        String entity = JsonRenderers.DEFAULT.render( ServerProperties.INSTANCE );
+        String entity = JsonRenderers.DEFAULT.render( properties );
 
         return addHeaders(
                 Response.ok( entity, JsonRenderers.DEFAULT.getMediaType() ) ).build();
 
     }
+
+    @POST
+    @Produces( MediaType.APPLICATION_JSON )
+    @Consumes( MediaType.APPLICATION_JSON )
+    @Path( "/" )
+    public Response jsonSetMany( String data )
+    {
+        return setMany( null );
+    }
+
+    @POST
+    @Produces( MediaType.APPLICATION_JSON )
+    @Consumes( MediaType.APPLICATION_FORM_URLENCODED )
+    @Path( "/" )
+    public Response formSetValueJSON( @FormParam( "value" ) String data )
+    {
+        return setMany( null );
+    }
+
+    // 
+    // INTERNALS
+    //
+
+    protected Response setMany( HashMap<String, String> values )
+    {
+        return null;
+    }
+
 }
