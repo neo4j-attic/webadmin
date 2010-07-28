07b417f10760409631c91b8d8a56b2815af97682
Jacob Hansson <jacob@bitchware.se>
2010-07-28 16:51:55 +0200

    Working on charting monitor data

--- src/main/resources/public/components/morpheus.server.monitor/js/MonitorChart.js
+++ src/main/resources/public/components/morpheus.server.monitor/js/MonitorChart.js
@@ -8,17 +8,57 @@ morpheus.components.server.monitor.MonitorChart = function(server) {
 	
 	var me = {};
 	
+	me.server = server;
 	me.containerId = "mor_monitor_chart_" + morpheus.components.server.monitor.monitorCharts++;
 	me.container = $("<div class='mor_module span-6'><h2>Monitor</h2><div id='" + me.containerId + "'></div></div>")
+	
+	me.drawing = false;
 	
 	me.public = {
 		
 		render : function() {
 		
+		},
+		
+		startDrawing : function() {
+			if ( ! me.drawing ) {
+				
+				me.drawing = true;
+				
+			}
+		},
+		
+		stopDrawing : function() {
+			me.drawing = false;
 		}
 			
 	};
 	
+	//
+	// INTERNALS
+	//
+	
+	me.draw = function(data) {
+		
+	};
+	
+	me.parseData = function(data) {
+		var out = [];
+		for( var i = 0, l = data.timestamps.length; i < l; i++ ) {
+			
+		}
+		console.log(out);
+		return out;
+	};
+	
+	// Listen for data updates
+	morpheus.event.bind("morpheus.server.monitor.update", function(ev) {
+		
+		if( ev.server === me.server ) {
+			me.draw( me.parseData(ev.allData) );
+		}
+		
+	});
 	
 	return me.public;
 };
