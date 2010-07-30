bf9665e680f2b7f48a7574be0394adf3a0c810dd
Jacob Hansson <jacob@bitchware.se>
2010-07-30 14:24:02 +0200

    Working on charts

--- src/main/resources/public/components/morpheus.server.monitor/js/MonitorChart.js
+++ src/main/resources/public/components/morpheus.server.monitor/js/MonitorChart.js
@@ -5,13 +5,36 @@ $.require("js/vend/jqplot-plugins/jqplot.dateAxisRenderer.js");
 
 morpheus.components.server.monitor.monitorCharts = 0;
 
-morpheus.components.server.monitor.MonitorChart = function(server) {
+/**
+ * A chart that shows data from one or more monitor data sources over time. The
+ * data is aquired via the morpheus.server.monitor.update event, which is
+ * triggered for each loaded server at regular intervals.
+ * 
+ * Available settings
+ * 
+ * @param server
+ *            is the server to work with
+ * @param settings
+ *            is an object that specifies what data to show and how. See above
+ *            for available settings.
+ */
+morpheus.components.server.monitor.MonitorChart = function(server, settings) {
 	
 	var me = {};
 	
+	// Default settings
+	me.settings = {
+		'label' : "",
+		'height' : 200,
+	};
+	
+	// Override defaults with user settings
+	var settings = settings || {};
+	$.extend( true, me.settings, settings );
+	
 	me.server = server;
 	me.containerId = "mor_monitor_chart_" + morpheus.components.server.monitor.monitorCharts++;
-	me.container = $("<div class='mor_module mor_span-5'><h2>Data load</h2><div class='mor_chart_container'><div style='height:200px;' id='" + me.containerId + "'></div></div></div>")
+	me.container = $("<div class='mor_module mor_span-5'><h2>" + me.settings.label + "</h2><div class='mor_chart_container'><div style='height:"+me.settings.height+"px;' id='" + me.containerId + "'></div></div></div>")
 	
 	me.drawing = false;
 	
@@ -77,7 +100,7 @@ morpheus.components.server.monitor.MonitorChart = function(server) {
 		
 		for( var i = 0, l = data.timestamps.length; i < l; i++ ) {
 			nodeCount.push([ data.timestamps[i], data.data["node_count"][i] ]);
-			relCount.push([ data.timestamps[i], data.data["relationship_count"][i] ]);
+			relCount.push([ data.timestamps[i],  data.data["relationship_count"][i] ]);
 			propCount.push([ data.timestamps[i], data.data["property_count"][i] ]);
 		}
 		
